505a5d45f11d143f237fd3d8f8e4d810c34e3ab7
Lynette Mwangi <lynettemwangi@Lynettes-MacBook-Pro.local>
2024-04-23 13:21:12 +0300

    docs: updated readme

--- packages/react-app/components/Header.tsx
+++ packages/react-app/components/Header.tsx
@@ -69,7 +69,7 @@ export default function Header() {
                                     </a>
                                 </div>
                             </div>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 {!hideConnectBtn && (
                                     <ConnectButton
                                         showBalance={{
@@ -78,7 +78,7 @@ export default function Header() {
                                         }}
                                     />
                                 )}
-                            </div>
+                            </div> */}
                         </div>
                     </div>
 
